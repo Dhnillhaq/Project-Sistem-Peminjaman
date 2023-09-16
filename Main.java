c146f7a21cad8761d1980979af2645e2002ea1a3
Khip01 <aakhifathallah24@gmail.com>
2023-09-16 20:25:09 +0700

    Add Option 2 Template Team

--- Main.java
+++ Main.java
@@ -69,6 +69,35 @@ public class Main {
 
     }
 
-    
+    //// METHOD AREA ////
+
+    public void loginRegister2() {
+        // Isi dari method
+    }
+
+    public void meminjamBuku2() {
+
+        // Deklarasi Scanner untuk input 
+        Scanner input = new Scanner(System.in);
+
+        // Deklarasi stok buku akademik
+        int stokBukuNonAkademik = 10;
+
+        // Sambutan
+        System.out.println("---------------------------------");
+        System.out.println("SELAMAT DATANG DI PEMINJAMAN BUKU\n");
+
+        // User menentukan jumlah peminjaman buku
+        System.out.println("[stok "+stokBukuNonAkademik+"]\nBuku Non-Akademik\n");
+        System.out.print("Jumlah buku yang ingin dipinjam?\n$> ");
+        int pilihBuku = input.nextInt();
+
+        // Melakukan pengurangan 
+        stokBukuNonAkademik -= pilihBuku;
+
+        // Menampilkan hasil dari peminjaman
+        System.out.println("\n[stok "+stokBukuNonAkademik+"]\nBuku Non-Akademik\n");
+
+    }
 
 }
